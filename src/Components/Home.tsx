92ed3113add6adc77ba3898e8457ce8c025526f2
Wania-Kazmi <waniakz019@gmail.com>
2023-06-11 14:42:09 +0500

    update

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,16 +4,14 @@ import AppContext from '../context/appContext';
 
 export default function Home() {
     const {isAuth, message, setIsAuth} = useContext(AppContext);
-    if(isAuth){
-        setIsAuth(false);
-    }
     console.log("message is: ",message);
     console.log("value of is auth is: ",isAuth)
   return (
 
-    <div className={`${isAuth ? 'bg-sky-400':'bg-red-300'}`}>
+    <div className={`${isAuth ? 'bg-sky-400':'bg-red-300'} flex flex-col gap-3`}>
         <h1 className='font-bold text-xl'>This is Home Page</h1>
         <p className='font-bold text-4xl'>{message}</p>
+        {isAuth ? (<button className='bg-gray-300' onClick={()=> setIsAuth(false)}>Logout</button>) : (<button className='bg-gray-300' onClick={()=> setIsAuth(true)}>Login</button>)}
     </div>
   )
 }
